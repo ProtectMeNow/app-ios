c1f568617b47c7fdeb8fec9780e641ec2e57137a
Erik Santos <erikgsj@hotmail.com>
2023-08-17 21:06:11 -0300

    Initial Commit

---
+++ ProtectMeNow/ProtectMeNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtectMeNowApp.swift
+//  ProtectMeNow
+//
+//  Created by Erik Santos on 17/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ProtectMeNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
